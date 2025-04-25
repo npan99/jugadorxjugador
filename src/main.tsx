3726c82a053e8d0b6283bbcbd19b4ace931d631a
npan99 <npan@futbolsites.net>
2025-04-25 12:06:24 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import PlayerRatingWidget from './PlayerRatingWidget';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
-    <App />
+    <PlayerRatingWidget />
   </React.StrictMode>
 );
