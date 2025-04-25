bb92aa723e340ad96f087ea18400f2d83b439359
npan99 <npan@futbolsites.net>
2025-04-25 00:59:59 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import PlayerRatingWidget from './PlayerRatingWidget';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', marginTop: 20 }}>Jugador x Jugador</h1>
+      <PlayerRatingWidget />
+    </div>
+  );
+}
+
+export default App;
